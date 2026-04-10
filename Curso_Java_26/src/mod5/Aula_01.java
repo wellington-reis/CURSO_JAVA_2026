7159ea30c07db2e5ad42f329b60a460b937177d9
Welligton Pc Gamer <Welligton Pc Gamer@DESKTOP-PVN4IRM>
2026-04-10 09:08:03 -0300

    Meu Primeiro Commit

---
+++ Curso_Java_26/src/mod5/Aula_01.java
@@ -0,0 +1,10 @@
+package mod5;
+
+public class Aula_01 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Word !!!!! - Meu Primeiro projeto java");
+	}
+
+}
---
+++ Curso_Java_26/src/mod6_Github/Aula_01_a_12.java
@@ -0,0 +1,31 @@
+package mod6_Github;
+
+public class Aula_01_a_12 {
+	
+	public static void main(String[] args) {
+		
+		/*aula01-
+		 *O que é Guithub - E uma Plataforma de versionamento de código 
+		 * 
+		 *
+		==============fim aula==========*/
+		
+		/*Aula02-Comandos mais ultilizados
+		 *Commit - Salvar alteracçoes no repositorio local (PC). 
+		 *Commit and Push - Salvar Alteraçoes tanto local tbm remoto.
+		 *Clonar - Copiar o Repositorio remoto ao local.
+		 *Revert - reverte o codigo local para o codigo remoto.
+		 *GitIgnore - ignora arquivos que não poder ser enviados ao repoistorio remoto. 
+		 *Pull - Atualiza o Codigo com as mudanças no repositorio remoto. 
+		 
+		 ==============Fim Aula=============*/
+		
+		/*Aula 03 - Criando um repositorio remoto e o Clonando
+		 * 
+		 ==============Fim Aula============*/
+		
+		/*Aula 04 - Realizando primeiro Commit e Commit and Push*/
+		
+		System.out.println("Realizando meu primeiro commit");/*Realizar o commit*/
+	}
+}
