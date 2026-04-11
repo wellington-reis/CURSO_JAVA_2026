12a003d510fe2f0b5ce78fa4e21cc72338573507
Welligton Pc Gamer <Welligton Pc Gamer@DESKTOP-PVN4IRM>
2026-04-11 17:00:20 -0300

    add nova aula

---
+++ Curso_Java_26/src/mod7_Tipos_Variaves/Aula7_Tipos_Texto.java
@@ -0,0 +1,43 @@
+package mod7_Tipos_Variaves;
+
+public class Aula7_Tipos_Texto {
+	
+	/*String = Texto de qualquer tamanho e configuração 
+	 *Char = Texto de apenas um Caracter.*/
+	
+	
+	public static void main(String[] args) {
+		
+		/*Criar um Cadastro de Pessoa com String*/
+		
+		String nomeAluno = "Wellington Reis";
+		String Endereço = "Rua Desembargador Braulio";
+		String Telefone ="31 994164940";
+		
+		/*Imprimir no console*/
+		System.out.println("Nome do Aluno: "+ nomeAluno );
+		System.out.println("Endereço: " + Endereço);
+		System.out.println("telefone: " + Telefone);
+		
+		/*Verificar o genero da Pessoa com "CHAR", onde (m) masculino (f) feminino
+		 * e qualquer outra letra Nao indentificado */
+		
+		char genero = 'a';/*Adicionar a Letra Aqui*/
+		
+		
+		/*Verificação de Condição*/
+		if (genero!='m' & genero !='f') {
+			
+			System.out.println("Sexo: Não Indentificado");
+			
+		}else if (genero == 'f') {
+			
+				 System.out.println("Sexo: Feminino");
+					
+			}else {
+				
+				 System.out.println("Sexo :Masculino ");
+					
+		}
+	}
+}
