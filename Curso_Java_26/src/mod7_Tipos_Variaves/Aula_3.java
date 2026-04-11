1aacdc7ef8b48b4b0e0e7ea4024ed4335cc85dd6
Welligton Pc Gamer <Welligton Pc Gamer@DESKTOP-PVN4IRM>
2026-04-11 12:22:03 -0300

    Adicionando modulo 7 e aula3 e nova branch

---
+++ Curso_Java_26/src/mod7_Tipos_Variaves/Aula_3.java
@@ -0,0 +1,24 @@
+package mod7_Tipos_Variaves;
+
+public class Aula_3 {
+	
+	/*Tipos Primitivos = Usados em todas as linguagens
+	 
+	 *tipos Numéricos = Números inteiros, racionais, decimais, etc.
+	 *tipos Strings = Textos .
+	 *tipos Booleanos = Logicos .
+	 *tipos Doubles =
+	 *tipos char =
+	
+	*/
+	
+	/*tipos numéricos:
+	 * ####   Tamanho(bits) - Faixa 	- Valor Padrão
+	 * Byte - 	8 bits 	- (-128 a 127) -	  0
+	 * Shots - 16 bits -(-32.768 a 32.767) - 0 
+	 * Int - 32 bits - (-2 elevado 31 a 2 elevado 31 - 1) - 0 
+	 * Long - 64 bits - ( -2 elevado 63 a 2 elevado 63 -1)- OL
+	*/
+	
+
+}
