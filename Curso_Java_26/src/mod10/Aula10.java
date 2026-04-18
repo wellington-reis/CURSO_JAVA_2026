83f32d4e02d3b6df47aab5139282c44bb797bb76
Welligton Pc Gamer <Welligton Pc Gamer@DESKTOP-PVN4IRM>
2026-04-18 16:53:59 -0300

    teste5

--- Curso_Java_26/src/mod10/Aula10.java
+++ Curso_Java_26/src/mod10/Aula10.java
@@ -5,9 +5,8 @@ public class Aula10 {
 	public static void main(String[] args) {
 		
 		System.out.println("teste");
-		System.out.println("teste2");
-		System.out.println("teste3");
-		System.out.println("teste4");
+	
+		
 		
 		
 	}
